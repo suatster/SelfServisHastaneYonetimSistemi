d67dcbadcbd6a80103ae4a0ee3035e24426f7d64
AbdiAciz <tahadogmac@hotmail.com>
2025-05-13 06:29:48 +0300

    Doktor listesi ile database bağlantısı yapıldı.

--- SelfServisHastaneYonetimSistemi/src/application/TRDoktorList1Controller.java
+++ SelfServisHastaneYonetimSistemi/src/application/TRDoktorList1Controller.java
@@ -1,6 +1,10 @@
 package application;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -27,51 +31,83 @@ public class TRDoktorList1Controller {
 
 	@FXML
 	private void initialize() {
-		Doktor[] doktorlar = {
-			new Doktor(123, "ahmetc1980@gmail.com", "Dr. Ahmet Çaydanlık", "05123456789", "Agiz"),
-			new Doktor(110, "ymine@gmail.com", "Dr. Mine Yavuz", "05123456788", "Goz")
-		};
+//		Doktor[] doktorlar = {
+//			new Doktor(123, "ahmetc1980@gmail.com", "Dr. Ahmet Çaydanlık", "05123456789", "Agiz"),
+//			new Doktor(110, "ymine@gmail.com", "Dr. Mine Yavuz", "05123456788", "Goz")
+//		};
+//
+//
+//		for (Doktor d : doktorlar) {
+//			switch (d.getAlan().toLowerCase()) {
+//			case "agiz":
+//				agizObs.add(d.getIsim());
+//				break;
+//
+//			case "aile":
+//				aileObs.add(d.getIsim());
+//				break;
+//
+//			case "beyin":
+//				beyinObs.add(d.getIsim());
+//				break;
+//
+//			case "cocuk":
+//				cocukObs.add(d.getIsim());
+//				break;
+//
+//			case "dermatoloji":
+//				dermatolojiObs.add(d.getIsim());
+//				break;
+//
+//			case "fizyoloji":
+//				fizyolojiObs.add(d.getIsim());
+//				break;
+//
+//			case "gogus":
+//				gogusObs.add(d.getIsim());
+//				break;
+//
+//			case "goz":
+//				gozObs.add(d.getIsim());
+//				break;
+//			}
+//		}
 
+		Connection conn = DatabaseConnection.connect();
+		if (conn == null) {
+			System.out.println("Veritabanı bağlantısı başarısız.");
+			return;
+		}
 
-		for (Doktor d : doktorlar) {
-			switch (d.getAlan().toLowerCase()) {
-			case "agiz":
-				agizObs.add(d.getIsim());
-				break;
-				
-			case "aile":
-				aileObs.add(d.getIsim());
-				break;
-			
-			case "beyin":
-				beyinObs.add(d.getIsim());
-				break;
-			
-			case "cocuk":
-				cocukObs.add(d.getIsim());
-				break;
-			
-			case "dermatoloji":
-				dermatolojiObs.add(d.getIsim());
-				break;
-				
-			case "fizyoloji":
-				fizyolojiObs.add(d.getIsim());
-				break;
-			
-			case "gogus":
-				gogusObs.add(d.getIsim());
-				break;
-			
-			case "goz":
-				gozObs.add(d.getIsim());
-				break;
+		try {
+			String sql = "SELECT * FROM doktor";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				String isim = rs.getString("isim");
+				String alan = rs.getString("alan");
+
+				switch (alan.toLowerCase()) {
+					case "agiz": agizObs.add(isim); break;
+					case "aile": aileObs.add(isim); break;
+					case "beyin": beyinObs.add(isim); break;
+					case "cocuk": cocukObs.add(isim); break;
+					case "dermatoloji": dermatolojiObs.add(isim); break;
+					case "fizyoloji": fizyolojiObs.add(isim); break;
+					case "gogus": gogusObs.add(isim); break;
+					case "goz": gozObs.add(isim); break;
+				}
 			}
+
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		agiz.setItems(agizObs);
 		aile.setItems(aileObs);
-		beyin.setItems(beyinObs); 
+		beyin.setItems(beyinObs);
 		cocuk.setItems(cocukObs);
 		dermatoloji.setItems(dermatolojiObs);
 		fizyoloji.setItems(fizyolojiObs);
--- SelfServisHastaneYonetimSistemi/src/application/TRDoktorList2Controller.java
+++ SelfServisHastaneYonetimSistemi/src/application/TRDoktorList2Controller.java
@@ -1,6 +1,10 @@
 package application;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -27,49 +31,81 @@ public class TRDoktorList2Controller {
 
 	@FXML
 	private void initialize() {
-		Doktor[] doktorlar = {
-			new Doktor(245, "finnmcmissile@gmail.com", "Prof. Finn McRoket", "05123456787", "Dahiliye"),
-			new Doktor(215, "hudson.eskitayfa@gmail.com", "Ord. Prof. Hudson Hornet", "05123456786", "Uroloji")
-		};
+//		Doktor[] doktorlar = {
+//			new Doktor(245, "finnmcmissile@gmail.com", "Prof. Finn McRoket", "05123456787", "Dahiliye"),
+//			new Doktor(215, "hudson.eskitayfa@gmail.com", "Ord. Prof. Hudson Hornet", "05123456786", "Uroloji")
+//		};
+//
+//		ObservableList<String> dahiliyeObs = FXCollections.observableArrayList();
+//
+//		for (Doktor d : doktorlar) {
+//			switch (d.getAlan().toLowerCase()) {
+//				case "dahiliye":
+//					dahiliyeObs.add(d.getIsim());
+//					break;
+//
+//				case "jinekoloji":
+//					jinekolojiObs.add(d.getIsim());
+//					break;
+//
+//				case "kalp":
+//					kalpObs.add(d.getIsim());
+//					break;
+//
+//				case "kulak":
+//					kulakObs.add(d.getIsim());
+//					break;
+//
+//				case "noroloji":
+//					norolojiObs.add(d.getIsim());
+//					break;
+//
+//				case "ortopedi":
+//					ortopediObs.add(d.getIsim());
+//					break;
+//
+//				case "ruh":
+//					ruhObs.add(d.getIsim());
+//					break;
+//
+//				case "uroloji":
+//					urolojiObs.add(d.getIsim());
+//					break;
+//			}
+//		}
 
-		ObservableList<String> dahiliyeObs = FXCollections.observableArrayList();
-
-		for (Doktor d : doktorlar) {
-			switch (d.getAlan().toLowerCase()) {
-				case "dahiliye":
-					dahiliyeObs.add(d.getIsim());
-					break;
-				
-				case "jinekoloji":
-					jinekolojiObs.add(d.getIsim());
-					break;
-				
-				case "kalp":
-					kalpObs.add(d.getIsim());
-					break;
-				
-				case "kulak":
-					kulakObs.add(d.getIsim());
-					break;
-				
-				case "noroloji":
-					norolojiObs.add(d.getIsim());
-					break;
-				
-				case "ortopedi":
-					ortopediObs.add(d.getIsim());
-					break;
-				
-				case "ruh":
-					ruhObs.add(d.getIsim());
-					break;
-				
-				case "uroloji":
-					urolojiObs.add(d.getIsim());
-					break;
-			}
+		Connection conn = DatabaseConnection.connect();
+		if (conn == null) {
+			System.out.println("Veritabanı bağlantısı başarısız.");
+			return;
 		}
-		
+
+		try {
+			String sql = "SELECT * FROM doktor";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				String isim = rs.getString("isim");
+				String alan = rs.getString("alan");
+
+				switch (alan.toLowerCase()) {
+					case "dahiliye": dahiliyeObs.add(isim); break;
+					case "jinekoloji": jinekolojiObs.add(isim); break;
+					case "kalp": kalpObs.add(isim); break;
+					case "kulak": kulakObs.add(isim); break;
+					case "noroloji": norolojiObs.add(isim); break;
+					case "ortopedi": ortopediObs.add(isim); break;
+					case "ruh": ruhObs.add(isim); break;
+					case "uroloji": urolojiObs.add(isim); break;
+				}
+			}
+
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		dahiliye.setItems(dahiliyeObs);
 		jinekoloji.setItems(jinekolojiObs);
 		kalp.setItems(kalpObs);
