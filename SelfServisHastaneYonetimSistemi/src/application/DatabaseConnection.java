5155a791134deb3c51aeadf29491698bad6c8302
AbdiAciz <tahadogmac@hotmail.com>
2025-05-13 04:28:23 +0300

    Düzeltme

---
+++ SelfServisHastaneYonetimSistemi/src/application/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String DB_URL = "jdbc:sqlite:hastane.db";
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DB_URL);
+            System.out.println("Veritabanına bağlandı!");
+        } catch (SQLException e) {
+            System.out.println("Bağlantı hatası: " + e.getMessage());
+        }
+        return conn;
+    }
+}
